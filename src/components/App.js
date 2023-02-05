19d13d8c8b801ce56ae8bec42d5fb1c793b9bea0
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2023-02-05 22:40:30 +0530

    completed

---
+++ src/components/App.js
@@ -0,0 +1,124 @@
+import { useState, useEffect } from "react";
+import "../styles/App.scss";
+import StatusLine from "./StatusLine";
+import { CSVLink } from "react-csv";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    loadTasksFromLocalStorage();
+  }, []);
+
+  function addEmptyTask(status) {
+    const lastTask = tasks[tasks.length - 1];
+
+    let newTaskId = 1;
+
+    if (lastTask !== undefined) {
+      newTaskId = lastTask.id + 1;
+    }
+
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        id: newTaskId,
+        title: "",
+        description: "",
+        status: status,
+      },
+    ]);
+  }
+
+  function addTask(taskToAdd) {
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== taskToAdd.id;
+    });
+
+    let newTaskList = [...filteredTasks, taskToAdd];
+
+    setTasks(newTaskList);
+
+    saveTasksToLocalStorage(newTaskList);
+  }
+
+  function deleteTask(taskId) {
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== taskId;
+    });
+
+    setTasks(filteredTasks);
+
+    saveTasksToLocalStorage(filteredTasks);
+  }
+
+  function moveTask(id, newStatus) {
+    let task = tasks.filter((task) => {
+      return task.id === id;
+    })[0];
+
+    let filteredTasks = tasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    task.status = newStatus;
+
+    let newTaskList = [...filteredTasks, task];
+
+    setTasks(newTaskList);
+
+    saveTasksToLocalStorage(newTaskList);
+  }
+
+  function saveTasksToLocalStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  function loadTasksFromLocalStorage() {
+    let loadedTasks = localStorage.getItem("tasks");
+
+    let tasks = JSON.parse(loadedTasks);
+
+    if (tasks) {
+      setTasks(tasks);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div style={{ display: "flex", alignItems: "center", gap: "5px", display: "flex", alignItems: "center", justifyContent: "center", margin: "2rem 0" }}>
+        <h1>Task Management</h1><CSVLink filename="task_list" style={{ textDecoration: "none" }} data={tasks}><p className="download">Download.csv</p></CSVLink>
+      </div>
+      <main>
+        <section>
+          <StatusLine
+            tasks={tasks}
+            addEmptyTask={addEmptyTask}
+            addTask={addTask}
+            deleteTask={deleteTask}
+            moveTask={moveTask}
+            status="Backlog"
+          />
+          <StatusLine
+            tasks={tasks}
+            addEmptyTask={addEmptyTask}
+            addTask={addTask}
+            deleteTask={deleteTask}
+            moveTask={moveTask}
+            status="In Progress"
+          />
+          <StatusLine
+            tasks={tasks}
+            addEmptyTask={addEmptyTask}
+            addTask={addTask}
+            deleteTask={deleteTask}
+            moveTask={moveTask}
+            status="Done"
+          />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
